31c730731b40032d67850620881a8e35abf04a3b
Mohit Divya <ap150751@gmail.com>
2023-07-27 15:13:57 +0530

    second commit

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.post("/failure",function(req,res){
     res.redirect("/")
 })
 
-app.listen("3000",function(req,res){
+app.listen(process.env.PORT || "3000",function(req,res){
     console.log("This server is running on port 3000");
 });
 
